00535a044b04b8c1a3ab599afcdf2d7109cd6a32
EduardoRH83 <124706620+EduardoRH83@users.noreply.github.com>
2023-02-10 08:34:03 -0600

    Add files via upload

---
+++ 02 Code/01 Script CC/in_four_step_26_v1.m
@@ -0,0 +1,177 @@
+%% Solve an Autoregression Problem with External Input with NARX Neural
+% Input 4: Power, Solar irradiance, Wind Direction, Temperature.
+% Training Function: trainbr
+% initialization: YES
+% 26 step ahead
+% X9
+counter = 0;
+rmse_for = 500; %<---300
+goal_rmse_for = 300; %<---250
+iterations = 20;
+mse_array = zeros(iterations,1);
+rmse_array = zeros(iterations,1);
+timep = zeros(iterations,1);
+h = waitbar(0,'Please wait...');
+% set(h,'FaceColor',[1 0 0],'EdgeColor',[0 0 1],'Name','Please wait...');
+while ((rmse_for >= goal_rmse_for) & (counter < iterations))
+tic  
+clc
+counter = counter + 1;
+Steps = 26;
+Delays = 13;
+
+waitbar(counter / iterations,h,['Red neuronal ' num2str(counter) '/' num2str(iterations)]);
+
+input = CleanData(1:3300-Steps,[1 2 3 8]); %<=========================input
+target = CleanData(Steps + 1:3300,1);
+
+X = tonndata(input,false,false);
+T = tonndata(target,false,false);
+
+% Choose a Training Function
+% For a list of all training functions type: help nntrain
+% 'trainlm' is usually faster.
+% 'trainbr' takes longer but may be better for challenging problems.
+    % 'trainscg' uses less memory. Suitable in low memory situations.
+trainFcn = 'trainbr';
+
+inputDelays     = 1:Delays;
+feedbackDelays  = 1:Delays;
+hiddenLayerSize = 10;
+net = narxnet(inputDelays,feedbackDelays,hiddenLayerSize,'open',trainFcn);
+
+net.inputs{1}.processFcns = {'removeconstantrows','mapminmax'};
+net.inputs{2}.processFcns = {'removeconstantrows','mapminmax'};
+
+[x,xi,ai,t] = preparets(net,X,{},T);
+
+net.divideFcn = 'dividerand'; % Divide data randomly
+net.divideMode = 'time'; % Divide up every sample
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio   = 15/100;
+net.divideParam.testRatio  = 15/100;
+
+% Choose a Performance Function
+% For a list of all performance functions type: help nnperformance
+net.performFcn = 'mse'; % Mean Squared Error
+
+% Choose Plot Functions
+% For a list of all plot functions type: help nnplot
+% net.plotFcns = {'plotperform','plottrainstate', 'ploterrhist', ...
+%         'plotregression', 'plotresponse', 'ploterrcorr', 'plotinerrcorr'};
+
+% Train the Network
+[net,tr] = train(net,x,t,xi,ai);
+NET{counter} = net;
+
+% Test the Network
+y = net(x,xi,ai);
+e{counter} = gsubtract(t,y);
+performance = perform(net,t,y);
+
+% Recalculate Training, Validation and Test Performance
+trainTargets = gmultiply(t,tr.trainMask);
+valTargets   = gmultiply(t,tr.valMask);
+testTargets  = gmultiply(t,tr.testMask);
+trainPerformance = perform(net,trainTargets,y);
+valPerformance   = perform(net,valTargets,y);
+testPerformance  = perform(net,testTargets,y);
+
+% clear X;
+ l = (3430 - 3300)/Steps + 1;
+ Predicted = zeros(Steps,l);
+ 
+for j = 0:l-1   
+    End = j*Steps;
+    T = CleanData(Steps+1:3300 + End,1);
+    T = tonndata(T,false,false);
+    wb = waitbar(0,['Iterando...' num2str(j) '/' num2str(l-1)]);
+    set(wb,'Name','Progreso del cálculo');
+    for i = 1:Steps        
+        nets = removedelay(net);
+        [xs,xis,ais,ts] = preparets(nets,X,{},T);
+        ys = nets(xs,xis,ais);        
+        X = CleanData(1:3300 - Steps + End + i,[1 2 3 8]); %<=========================input
+        X = tonndata(X,false,false);
+        Predicted(i,j+1) = cell2mat(ys(end));
+        T(end+1) = con2seq(Predicted(i,j+1));
+        waitbar(i/Steps);
+    end  
+    delete(wb);
+end
+   
+    Predicted = Predicted(:);
+    Real = CleanData(3275:end,1);
+    n = size(Real,1);
+    
+    mse_for = meansqr(Predicted - Real);
+    rmse_for = sqrt(mse_for);
+    
+    mse_array(counter) = mse_for;
+    rmse_array(counter) = rmse_for;
+%     ======================================================================
+    net = init(net); %<=====================================
+%     ======================================================================
+%     plot(1:n,Predicted,1:n,Real)
+%     title(['MSE = ' num2str(mse_for,'%.2e')])
+%     legend('Forecasting','Real')
+%     nntraintool('close')   
+
+time = toc;
+
+timep(counter) = time; 
+end
+
+totaltime = sum(timep);
+
+timep = datestr(timep/86400,'HH:MM:SS.FFF');
+totaltime = datestr(totaltime/86400,'HH:MM:SS.FFF');
+delete(h);
+%% Plot the results
+
+input = CleanData(1:3300-Steps,[1 2 3 8]); %<=========================input
+target = CleanData(Steps + 1:3300,1);
+
+X = tonndata(input,false,false);
+T = tonndata(target,false,false);
+
+
+[~,index] = min(rmse_array);
+net = NET{index};
+
+ l = (3430 - 3300)/Steps + 1;
+ Predicted = zeros(Steps,l);
+ 
+for j = 0:l-1   
+    End = j*Steps;
+    T = CleanData(Steps+1:3300 + End,1);
+    T = tonndata(T,false,false);
+    wb = waitbar(0,['Iterando...' num2str(j) '/' num2str(l-1)]);
+    set(wb,'Name','Progreso del cálculo');
+    for i = 1:Steps        
+        nets = removedelay(net);
+        [xs,xis,ais,ts] = preparets(nets,X,{},T);
+        ys = nets(xs,xis,ais);        
+        X = CleanData(1:3300 - Steps + End + i,[1 2 3 8]); %<=========================input
+        X = tonndata(X,false,false);
+        Predicted(i,j+1) = cell2mat(ys(end));
+        T(end+1) = con2seq(Predicted(i,j+1));
+        waitbar(i/Steps);
+    end  
+    delete(wb);
+end
+
+    Predicted = Predicted(:);
+    Real = CleanData(3275:end,1);
+    n = size(Real,1);
+    
+    mse_for = meansqr(Predicted - Real);
+    rmse_for = sqrt(mse_for);
+    
+    mse_array(counter) = mse_for;
+    rmse_array(counter) = rmse_for;
+    
+    plot(1:n,Predicted,1:n,Real)
+    title(['MSE = ' num2str(mse_for,'%.2e')])
+    legend('Forecasting','Real')
+    nntraintool('close')  
